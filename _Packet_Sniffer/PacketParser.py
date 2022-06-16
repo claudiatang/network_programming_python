e3c002364e8caad71e032af90f29552d3033404e
claudiatang <23091097+claudiatang@users.noreply.github.com>
2022-06-16 17:49:02 +1000

    add tcp parser functions

--- _Packet_Sniffer/PacketParser.py
+++ _Packet_Sniffer/PacketParser.py
@@ -1,3 +1,4 @@
+from pickletools import long1
 import struct
 import socket
 
@@ -31,3 +32,39 @@ def get_ip_header(ip_header_20):
 def get_ip_addr(addr_bytes_obj):
     addr_sections = map(lambda x: format(x, 'd'), addr_bytes_obj)
     return '.'.join(addr_sections)
+
+def get_icmp_header(icmp_header_8):
+    icmp_header = struct.unpack('!BBHHH', icmp_header_8)
+    icmp_type = int(icmp_header[0])
+    icmp_code = int(icmp_header[1])
+    checksum = bin(icmp_header[2])
+    pid = int(icmp_header[3])
+    seq_num = int(icmp_header[4])
+    return icmp_type, icmp_code, checksum, pid, seq_num
+
+def get_tcp_header(tcp_header_20):
+    tcp_header = struct.unpack('!HHLLBBHHH', tcp_header_20)
+    src_port = int(tcp_header[0])
+    dest_port = int(tcp_header[1])
+    tcp_seq = int(tcp_header[2])
+    tcp_ack = int(tcp_header[3])
+    data_offset = bin(int((0b01111000000000000 & tcp_header[4])/4096))
+    reserved = bin(int((0b00000111000000000 & tcp_header[4])/512))
+    control_flags = get_tcp_flags(0b00000000111111111 & tcp_header[4])
+    win_size = int(tcp_header[5])
+    checksum = bin(tcp_header[6])
+    urg_pnt = tcp_header[7]
+    return src_port, dest_port, tcp_seq, tcp_ack, data_offset, reserved, control_flags, win_size, checksum, urg_pnt
+    
+def get_tcp_flags(flag_bits_9):
+    nonce =    bool(0b0100000000 & flag_bits_9)
+    cwr =      bool(0b0010000000 & flag_bits_9)
+    ecn_echo = bool(0b0001000000 & flag_bits_9)
+    urgent =   bool(0b0000100000 & flag_bits_9)
+    ack =      bool(0b0000010000 & flag_bits_9)
+    push =     bool(0b0000001000 & flag_bits_9)
+    reset =    bool(0b0000000100 & flag_bits_9)
+    syn =      bool(0b0000000010 & flag_bits_9)
+    fin =      bool(0b0000000001 & flag_bits_9)
+    return nonce, cwr, ecn_echo, urgent, ack, push, reset, syn, fin
+    
--- _Packet_Sniffer/PacketSniffer.py
+++ _Packet_Sniffer/PacketSniffer.py
@@ -4,7 +4,8 @@ import socket
 import PacketParser as pparser
 
 def main():
-    HOST = socket.gethostbyname(socket.gethostname())
+    HOST = socket.gethostbyname_ex(socket.gethostname())[-1][-1]
+    print(HOST)
 
     
     # create a raw socket and bind it to the public interface
@@ -25,16 +26,43 @@ def main():
             #dlHeader = get_ether_header(rawData)
             ipHeader = pparser.get_ip_header(rawData[:20])
             #print(f"data link layer header: {dlHeader}")
-            print("Network Layer header fields:")
-            print(f"   IP version: {ipHeader[0]}")
-            print(f"   header length: {ipHeader[1]} 32-bit words = {int((ipHeader[1]*32/8))} bytes")
-            print(f"   Packet total length: {ipHeader[3]}")
-            print(f"   TTL: {ipHeader[5]}")
-            print(f"   Upper layer protocol: {ipHeader[6]}")
-            print(f"   ip addresses: src:{ipHeader[7]} --> dest:{ipHeader[8]}")
-            #print(rawData)
+            print("# Network Layer header fields:")
+            print(f"  IP version: {ipHeader[0]}")
+            print(f"  header length: {ipHeader[1]} 32-bit words = {int((ipHeader[1]*32/8))} bytes")
+            print(f"  Packet total length: {ipHeader[3]}")
+            print(f"  TTL: {ipHeader[5]}")
+            print(f"  Upper layer protocol: {ipHeader[6]}")
+            print(f"  ip addresses: src:{ipHeader[7]} --> dest:{ipHeader[8]}")
+            
+            #print ICMP header
+            if int(ipHeader[6]) == 1:
+                icmp_header = pparser.get_icmp_header(rawData[20:28])
+                print("## ICMP header fields:")
+                print(f"   ICMP type: {icmp_header[0]}")
+                print(f"   ICMP code: {icmp_header[1]}")
+                print(f"   ICMP checksum: {icmp_header[2]} {len(icmp_header[2])}")
+                print(f"   pid: {icmp_header[3]}")
+                print(f"   sequence number: {icmp_header[4]}")
             # disabled promiscuous mode
             winRawSocket.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+            
+            #print TCP header
+            if int(ipHeader[6]) == 6:
+                tcp_header = pparser.get_tcp_header(rawData[20:40])
+                print("## TCP header fields:")
+                print(f"   TCP src port: {tcp_header[0]}")
+                print(f"   TCP dest port: {tcp_header[1]}")
+                print(f"   TCP seq num: {tcp_header[2]}")
+                print(f"   TCP ack num: {tcp_header[3]}")
+                print(f"   TCP data offset: {tcp_header[4]}")
+                print(f"   TCP reserved: {tcp_header[5]}")
+                print(f"   TCP flags:")
+                for flag, val in zip(["nonce", "cwr", "ecn_echo", "urgent", "ack", "push", "reset", "syn", "fin"], tcp_header[6]):
+                    print(f"     TCP flag {flag}: {val}")
+                print(f"   TCP window size: {tcp_header[7]}")
+                print(f"   TCP checksum: {tcp_header[8]}")
+                print(f"   TCP urgent point: {tcp_header[9]}")
+                
     except KeyboardInterrupt:
         pass
     
